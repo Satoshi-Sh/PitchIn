0427111e819bcb5730e835a1df9237e96237b469
satoshi-sh <satoss1108@gmail.com>
2024-01-09 14:25:53 -0600

    set up a starter and created navbar

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import NavigationBar from "./components/NavigationBar";
+function App() {
+  return (
+    <div className="App">
+      <NavigationBar />
+    </div>
+  );
+}
+
+export default App;
