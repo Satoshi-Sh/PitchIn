ea8ec64d1348ff668a386506472e7c3a0ab07d7e
satoshi-sh <satoss1108@gmail.com>
2024-01-09 16:13:33 -0600

    fixed the colors

--- client/src/components/IconContainer.js
+++ client/src/components/IconContainer.js
@@ -1,7 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const IconContainer = ({ icon }) => {
   return (
-    <li className="border border bg-blue-500 rounded-full w-12 h-12 p-2 flex justify-center items-center overflow-hidden">
+    <li className="border bg-blue-500 rounded-full w-12 h-12 p-2 flex justify-center items-center overflow-hidden">
       <FontAwesomeIcon icon={icon} className="text-2xl text-white" />
     </li>
   );
--- client/src/components/NavigationBar.js
+++ client/src/components/NavigationBar.js
@@ -13,7 +13,7 @@ const NavigationBar = () => {
           <div className="flex gap-3">
             <button
               type="button"
-              className=" border hover:bg-lime-300 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className=" border hover:bg-pink-300 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               Signup
             </button>
