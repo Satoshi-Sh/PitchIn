d92cac0d7abeec7e1148c788ba925a6659141195
satoshi-sh <satoss1108@gmail.com>
2024-01-24 16:29:43 -0600

    Added redirect to dashboard if user has token already

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,16 @@
 import AutoScroll from "../components/AutoScroll";
 import Hero from "../components/Hero";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 const Home = () => {
+  const navigate = useNavigate();
+  const { user } = useAuth0();
+  useEffect(() => {
+    if (user) {
+      navigate("/dashboard");
+    }
+  }, [user]);
   return (
     <div className="py-36 flex flex-col justify-between h-screen">
       <Hero />
